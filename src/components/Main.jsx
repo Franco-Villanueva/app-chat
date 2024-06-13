fe02494c536a768f38655411fe5345ade88cdcec
Franco-Villanueva <franco.villanueva.emanuel@gmail.com>
2024-06-12 21:23:52 -0300

    ReactNative

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Constants from "expo-constants";
+import { View, Text } from "react-native";
+
+const Main = () => {
+    return ( 
+        <View style={{marginTop:Constants.statusBarHeight , flexGrow:1}}>
+            <Text>Pruebas</Text>
+        </View>
+     );
+}
+ 
+export default Main;
---
+++ src/data/api.js
@@ -0,0 +1,52 @@
+export default [
+    {
+      "name": "John Doe",
+      "last": "Jane Smith",
+      "age": "1990"
+    },
+    {
+      "name": "Alice Johnson",
+      "last": "Michael Brown",
+      "age": "2005"
+    },
+    {
+      "name": "Emily Davis",
+      "last": "David Johnson",
+      "age": "2012"
+    },
+    {
+      "name": "Daniel Wilson",
+      "last": "Olivia Taylor",
+      "age": "1998"
+    },
+    {
+      "name": "Sophia Anderson",
+      "last": "William Davis",
+      "age": "2010"
+    },
+    {
+      "name": "Ava Martinez",
+      "last": "James Wilson",
+      "age": "2008"
+    },
+    {
+      "name": "Logan Thomas",
+      "last": "Ella Anderson",
+      "age": "2015"
+    },
+    {
+      "name": "Mia Clark",
+      "last": "Alexander Martinez",
+      "age": "2002"
+    },
+    {
+      "name": "Jacob Rodriguez",
+      "last": "Sofia Clark",
+      "age": "2006"
+    },
+    {
+      "name": "Oliver Lee",
+      "last": "Liam Rodriguez",
+      "age": "2018"
+    }
+  ]
